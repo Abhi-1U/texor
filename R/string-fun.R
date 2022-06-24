ad50f8b5e4df3ffc905f7d3be27215879eb5b0db
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-06-24 12:32:34 +0530

    Improved implementation

---
+++ R/string-fun.R
@@ -0,0 +1,37 @@
+#Wrapper functions for stringr package methods
+
+# -- stringr wrapper ----------------------------------------------------------
+
+str_trim<-function(x){
+    return(stringr::str_trim(x))
+}
+str_detect<-function(x,re){
+    return(stringr::str_detect(x,re))
+}
+str_match<-function(x,re){
+    return(stringr::str_match(x,re))
+}
+str_split<-function(x,patt){
+    return(stringr::str_split(x,patt))
+}
+str_length<-function(x){
+    return(stringr::str_length(x))
+}
+str_replace_all<-function(x,patt,rep){
+    return(stringr::str_replace_all(x,patt,rep))
+}
+fixed<-function(x){
+    return(stringr::fixed(x))
+}
+str_c<-function(x,sep){
+    return(stringr::str_c(x,sep))
+}
+empty <- function(x) UseMethod("empty")
+
+empty.character <- function(x) str_length(x) == 0
+#' @method empty address_list
+#' @export
+empty.address_list <- function(x) length(x) == 0
+#' @method empty NULL
+#' @export
+empty.NULL <- function(x) TRUE
