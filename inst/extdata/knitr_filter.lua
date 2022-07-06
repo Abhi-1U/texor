1cf1910b1597e12d446c175b548887c2a1ca3135
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-07-06 23:49:23 +0530

    Remove fig: from identifier which helps with knitr

--- inst/extdata/knitr_filter.lua
+++ inst/extdata/knitr_filter.lua
@@ -9,7 +9,7 @@ License:   MIT – see LICENSE file for details
 Creates a new pandoc.RawInline with R-markdown spec image block
 --]]
 function KnitrBlock(img)
-    local attr_construct='```{r ' .. pandoc.utils.stringify(img.attr.identifier) ..', echo=FALSE , fig.cap="'.. pandoc.utils.stringify(img.caption) .. '"}\n'
+    local attr_construct='```{r ' .. pandoc.utils.stringify(img.attr.identifier):gsub("^fig:", "") ..', echo=FALSE , fig.cap="'.. pandoc.utils.stringify(img.caption) .. '"}\n'
     local knitr_command='knitr::include_graphics("'.. img.src ..'")'
     return pandoc.RawInline('markdown', attr_construct .. knitr_command..'\n```\n')
 end
