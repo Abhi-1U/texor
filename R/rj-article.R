0436698e12238dc118a6ece2693c9738c5c92183
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-07-11 00:15:26 +0530

    Better man pages and reduce duplicate code

--- R/rj-article.R
+++ R/rj-article.R
@@ -1,99 +1,83 @@
-#' Include Style File
+#' Include a Metafix.sty style file
 #'
+#' This style file helps texor and pandoc to retain metadata and ease
+#' the conversion process.
 #' @param article_dir path to the directory which contains tex article
 #'
 #' @return
-#' @export
+#' @export Metafix.sty file in the article_dir
 #'
 #' @examples
-include_style_file<-function(article_dir){
+#' texor::include_style_file(".")
+include_style_file <- function(article_dir) {
     # Copy over the Metafix.sty file to the article directory
     file.copy(
             system.file("extdata/Metafix.sty", package = "texor"),
             file.path(article_dir),
         )
     # Modify LaTeX source to include the \include(Metafix) line
-    # If the wrapper file name is RJwrapper.tex
-    if(file.exists(file.path(article_dir, "RJwrapper.tex"))) {
-        wrapper_file <- readLines(file.path(article_dir, "RJwrapper.tex"))
-        doc_start <- which(grepl("^\\s*\\\\begin\\{document\\}", wrapper_file))
-        before_doc_start<- setdiff(wrapper_file[seq_len(doc_start)-1], '\n')
-        after_doc_start<-setdiff(wrapper_file,before_doc_start)
-        usepackage_metafix<-"\\usepackage{Metafix}"
-        # Backup original wrapper file
-        file.rename('RJwrapper.tex','RJwrapper.tex.bk')
+    file_name <- get_wrapper_type(article_dir)
+    wrapper_file <- readLines(file.path(article_dir, file_name))
+    doc_start <- which(grepl("^\\s*\\\\begin\\{document\\}", wrapper_file))
+    before_doc_start <- setdiff(wrapper_file[seq_len(doc_start) - 1], "\n")
+    after_doc_start <- setdiff(wrapper_file, before_doc_start)
+    usepackage_metafix <- "\\usepackage{Metafix}"
+    # Backup original wrapper file
+    file.rename(file_name, paste(file_name, ".bk", sep = ""))
         # write to original wrapper file
-        xfun::write_utf8(
-            c(before_doc_start,usepackage_metafix,"",after_doc_start),
-            "RJwrapper.tex.new")
+        xfun::write_utf8(c(
+            before_doc_start,
+            usepackage_metafix,
+            "",
+            after_doc_start),
+            paste(file_name, ".new", sep = ""))
         #
-        file.rename('RJwrapper.tex.new','RJwrapper.tex')
-    }
-    else{
-    # If the wrapper file name is RJwrap.tex
-        if(file.exists(file.path(article_dir, "RJwrap.tex"))) {
-            wrapper_file <- readLines(file.path(article_dir, "RJwrap.tex"))
-            doc_start <- which(grepl("^\\s*\\\\begin\\{document\\}", wrapper_file))
-            before_doc_start<- setdiff(wrapper_file[seq_len(doc_start)-1], '\n')
-            after_doc_start<-setdiff(wrapper_file,before_doc_start)
-            usepackage_metafix<-"\\usepackage{Metafix}"
-            # Backup original wrapper file
-            write_file<-file("RJwrap.tex.bk")
-            writeLines(wrapper_file, write_file)
-            close(write_file)
-            # write to original wrapper file
-            write_file<-file("RJwrap.tex",'w')
-            writeLines(c(before_doc_start,usepackage_metafix,"",after_doc_start), write_file)
-            close(write_file)
-        }
-    # No wrapper file found !
-    else {
-            print('RJwrapper.tex or RJwrap.tex does not exist !')
-        }
-    }
+        file.rename(paste(file_name, ".new", sep = ""), file_name)
 }
-#' convert to Rmarkdown
+
+#' convert latex(wrapper) file to markdown
 #'
-#' @param article_dir
+#' @param article_dir path to the directory which contains tex article
 #'
 #' @return
-#' @export
+#' @export creates a converted markdown file
 #'
 #' @examples
-convert_to_markdown<-function(article_dir){
-    # a precautionary measure to ensure that working directory is managed
-    if(file.exists(file.path(article_dir, "RJwrapper.tex"))) {
-        input_file='RJwrapper.tex'
-    }
-    else{
-        if(file.exists(file.path(article_dir, "RJwrap.tex"))) {
-            input_file='RJwrap.tex'
-        }
-        else{
-            print('RJwrapper.tex or RJwrap.tex does not exist !')
-        }
-    }
+#' texor::convert_to_markdown(".")
+convert_to_markdown <- function(article_dir) {
+    input_file <- get_wrapper_type(article_dir)
     print(input_file)
-    #library(tools)
-    abs_file_path<-tools::file_path_as_absolute(article_dir)
-    md_file=paste(toString(tools::file_path_sans_ext(input_file)),".md",sep="")
+    abs_file_path <- tools::file_path_as_absolute(article_dir)
+    md_file <- paste(toString(tools::file_path_sans_ext(input_file)),
+                             ".md", sep = "")
     print(md_file)
-    bib_filter<-system.file("extdata/bib_filter.lua", package = "texor")
-    code_block_filter<-system.file("extdata/code_block_filter.lua", package = "texor")
-    image_filter<-system.file("extdata/image_filter.lua", package = "texor")
-    post_tikz_filter<-system.file("extdata/reinstate_tikz_filter.lua", package = "texor")
-    knitr_filter<-system.file("extdata/knitr_filter.lua",package ="texor")
-    pandoc_opt<-c("-s",
-                  "--resource-path",abs_file_path,
-                  "--lua-filter",bib_filter,
-                  "--lua-filter",image_filter,
-                  "--lua-filter",code_block_filter,
-                  "--lua-filter",knitr_filter,
-                  "--lua-filter",post_tikz_filter)
+    bib_filter <- system.file(
+                "extdata/bib_filter.lua", package = "texor")
+    code_block_filter <- system.file(
+                "extdata/code_block_filter.lua", package = "texor")
+    image_filter <- system.file(
+                "extdata/image_filter.lua", package = "texor")
+    post_tikz_filter <- system.file(
+                "extdata/reinstate_tikz_filter.lua", package = "texor")
+    knitr_filter <- system.file(
+                "extdata/knitr_filter.lua",package = "texor")
+    pandoc_opt <- c("-s",
+                  "--resource-path", abs_file_path,
+                  "--lua-filter", bib_filter,
+                  "--lua-filter", image_filter,
+                  "--lua-filter", code_block_filter,
+                  "--lua-filter", knitr_filter,
+                  "--lua-filter", post_tikz_filter)
     #output_format<-"markdown-simple_tables-multiline_tables-pipe_tables"
-    output_format<-"markdown-simple_tables-pipe_tables"
+    output_format <- "markdown-simple_tables-pipe_tables"
     # This will generate a markdown file with YAML headers.
-    rmarkdown::pandoc_convert(input_file,from = "latex",to= output_format,options=pandoc_opt,output = md_file,citeproc = TRUE,verbose = TRUE)
+    rmarkdown::pandoc_convert(input_file,
+                              from = "latex",
+                              to = output_format,
+                              options = pandoc_opt,
+                              output = md_file,
+                              citeproc = TRUE,
+                              verbose = TRUE)
 }
 
 #' generate rmarkdown file in output folder
@@ -103,12 +87,13 @@ convert_to_markdown<-function(article_dir){
 #' @param issue issue number (int)
 #'
 #' @return
-#' @export
+#' @export .Rmd file in the /output folder
 #'
 #' @examples
-generate_rmd<-function(markdown_file,volume, issue){
+#' texor::generate_rmd("/2009-05/RJwrapper.md", 1, 1)
+generate_rmd <- function(markdown_file, volume, issue) {
     metadata <- rmarkdown::yaml_front_matter(markdown_file)
-    metadata$abstract<- metadata$author[2]
+    metadata$abstract <- metadata$author[2]
     #metadata$emails<-metadata$author[3:length(metadata$author)]
     metadata$author <- lapply(
             strsplit(metadata$address, "\\\n", fixed = TRUE),
@@ -117,10 +102,10 @@ generate_rmd<-function(markdown_file,volume, issue){
                     name = person[1],
                     affiliation = person[2]
                 )
-                if(any(orcid <- grepl("^ORCiD:", person))) {
+                if (any(orcid <- grepl("^ORCiD:", person))) {
                     author$orcid <- sub("^ORCiD: ", "", person[orcid])
                 }
-                if(any(email <- grepl("^email:", person))) {
+                if (any(email <- grepl("^email:", person))) {
                     author$email <- sub("^email:", "", person[email])
                 }
                 #if(!is.na(metadata$emails)){
@@ -128,7 +113,7 @@ generate_rmd<-function(markdown_file,volume, issue){
                 #}
                 fields <- logical(length(person))
                 fields[1:2] <- TRUE
-                if(any(address <- !(fields | orcid | email))) {
+                if (any(address <- !(fields | orcid | email))) {
                     author$address <- person[address]
                 }
                 author
@@ -137,7 +122,8 @@ generate_rmd<-function(markdown_file,volume, issue){
     metadata$address <- NULL
 
     # article metadata from DESCRIPTION
-    article_metadata <- if(file.exists(file.path(dirname(markdown_file), "DESCRIPTION"))) {
+    article_metadata <- if (file.exists(file.path(
+            dirname(markdown_file), "DESCRIPTION"))) {
         art <- load_article(file.path(dirname(markdown_file), "DESCRIPTION"))
         online_date <- Filter(function(x) x$status == "online", art$status)
         online_date <- if (length(online_date) == 0) {
@@ -155,10 +141,11 @@ generate_rmd<-function(markdown_file,volume, issue){
     } else {
         # Produce minimal article metadata for news
         issue_year <- volume + 2008
-        issue_month <- if(issue_year < 2022) issue * 6 else issue * 3
+        issue_month <- if (issue_year < 2022) issue * 6 else issue * 3
         list(
             slug = basename(dirname(markdown_file)),
-            online = as.Date(paste(volume + 2008, issue_month, "01"), format = "%Y %m %d")
+            online = as.Date(paste(volume + 2008,
+            issue_month, "01"), format = "%Y %m %d")
         )
     }
     front_matter <- list(
@@ -178,14 +165,14 @@ generate_rmd<-function(markdown_file,volume, issue){
             cran = article_metadata$CRANpkgs,
             bioc = article_metadata$BIOpkgs
         ),
-        preview = 'preview.png',
+        preview = "preview.png",
         bibliography = metadata$bibliography,
         CTV = article_metadata$CTV_rev,
         output = list(
             `rjtools::rjournal_web_article` = list(
                 self_contained = FALSE,
                 toc = FALSE,
-                legacy_pdf = TRUE
+                legacy_pdf = FALSE
             )
             # `rjtools::rjournal_pdf_article` = pdf_args
         )
@@ -198,13 +185,15 @@ generate_rmd<-function(markdown_file,volume, issue){
     if (delimiters[2] < length(pandoc_md_contents))
         article_body <- c(article_body, pandoc_md_contents[-(1:delimiters[2])])
 
-    input_file=basename(markdown_file)
-    template_path=paste(find.package('texor'),'extdata/rmd-style-markdown.template',sep ='/')
-    output_file_name=paste(dirname(markdown_file),"/output/",toString(tools::file_path_sans_ext(input_file)),".Rmd",sep="")
-    dir.create(dirname(output_file_name),showWarnings = F)
+    input_file <- basename(markdown_file)
+    template_path <- paste(find.package("texor"),
+                "extdata/rmd-style-markdown.template", sep = "/")
+    output_file_name <- paste(dirname(markdown_file),
+                                    "/output/",
+                            toString(tools::file_path_sans_ext(input_file)),
+                                    ".Rmd", sep = "")
+    dir.create(dirname(output_file_name), showWarnings = FALSE)
     xfun::write_utf8(
         c("---", yaml::as.yaml(front_matter), "---", article_body),
         output_file_name)
 }
-
-
--- R/tikz-tools.R
+++ R/tikz-tools.R
@@ -1,25 +1,13 @@
 manage_tikz_images<-function(article_dir){
-    old_wd<-getwd()
-    if(getwd()!=article_dir){
+    if (getwd()!= article_dir) {
         setwd(article_dir)
     }
     # checking for RJwrapper and fetching the file name for tex file
-    wrapper_types=c('wrapper.tex','RJwrap.tex','RJwrapper.tex')
-    wrapper_file=""
-    for(w_type in wrapper_types){
-        if(file.exists(file.path(article_dir, w_type))) {
-            print(paste("Tikz-tools Stage 1 : Found ",w_type))
-            wrapper_file=w_type
-        }
-    }
-    file_name<-find_src_file(article_dir,wrapper_file)
-    if(!grepl(".tex$",file_name)){
-        file_name<-paste0(file_name,".tex")
-    }
+    file_name <- get_texfile_name(article_dir)
     # extract tikz blocks as objects
-    tikz_object=extract_embeded_tikz_image(article_dir,file_name)
+    tikz_object <- extract_embeded_tikz_image(article_dir, file_name)
     # isolate tikz into a template latex file
-    tikz_template<-c(
+    tikz_template <- c(
         "\\documentclass{standalone}",
         "\\usepackage{xcolor}",
         "\\usepackage{verbatim}",
@@ -33,117 +21,108 @@ manage_tikz_images<-function(article_dir){
         tikz_object,
         "\\end{document}"
     )
-    fileConn<-file("tikz.tex")
-    writeLines(tikz_template, fileConn)
-    close(fileConn)
+    fileconn <- file("tikz.tex")
+    writeLines(tikz_template, fileconn)
+    close(fileconn)
     # convert the tex file into pdf
-    dir.create('tikz',showWarnings = F)
-    tikz_dir<-paste(article_dir,'tikz',sep='/')
-    file.copy('tikz.tex',tikz_dir,copy.mode=T,recursive=F)
-    tinytex::latexmk('tikz/tikz.tex',engine = 'pdflatex')
+    dir.create("tikz", showWarnings = FALSE)
+    tikz_dir <- paste(article_dir, "tikz", sep = "/")
+    file.copy("tikz.tex", tikz_dir, copy.mode = TRUE, recursive = FALSE)
+    tinytex::latexmk("tikz/tikz.tex", engine = "pdflatex")
     # run pdf to png
-    texor::make_png_files('','./tikz/tikz.pdf')
+    texor::make_png_files("./tikz/tikz.pdf")
     # copy over the file
-    texor::Copy_Other_Files('.')
+    texor::Copy_Other_Files(".")
     # include tikz as pdf in the tex document
-    image_path<-'tikz/tikz.png'
-    texor::inject_generated_image(article_dir,file_name,image_path)
-
+    image_path <- "tikz/tikz.png"
+    texor::inject_generated_image(article_dir, file_name, image_path)
 }
 
-extract_embeded_tikz_image<-function(article_dir,file_name){
-    print(paste("TKZ-S2 : extracting Tikz Code from ",file_name))
-    src_file_data<- readLines(file.path(article_dir,file_name))
-    fig_start<- which(grepl("^\\s*\\\\begin\\{figure", src_file_data))
-    fig_end<- which(grepl("^\\s*\\\\end\\{figure", src_file_data))
-    pre_fig<- src_file_data[seq_len(fig_start)-1]
-    post_fig<-src_file_data[seq_len(fig_end)]
-    fig_data<-setdiff(post_fig,pre_fig)
-    tikz_start<- which(grepl("^\\s*\\\\begin\\{tikzpicture", fig_data))
-    tikz_end<- which(grepl("^\\s*\\\\end\\{tikzpicture", fig_data))
-    tikz_style<- which(grepl("^\\s*\\\\tikzstyle\\{", fig_data))
+extract_embeded_tikz_image<-function(article_dir, file_name){
+    print(paste("TKZ-S2 : extracting Tikz Code from ", file_name))
+    src_file_data <- readLines(file.path(article_dir, file_name))
+    fig_start <- which(grepl("^\\s*\\\\begin\\{figure", src_file_data))
+    fig_end <- which(grepl("^\\s*\\\\end\\{figure", src_file_data))
+    pre_fig <- src_file_data[seq_len(fig_start) - 1]
+    post_fig <- src_file_data[seq_len(fig_end)]
+    fig_data <- setdiff(post_fig,pre_fig)
+    tikz_start <- which(grepl("^\\s*\\\\begin\\{tikzpicture", fig_data))
+    tikz_end <- which(grepl("^\\s*\\\\end\\{tikzpicture", fig_data))
+    tikz_style <- which(grepl("^\\s*\\\\tikzstyle\\{", fig_data))
     # if no tikz its probably a PDF or PNG image
-    if(identical(tikz_start,integer(0))||identical(tikz_end,integer(0))){
+    if (identical(tikz_start, integer(0)) || identical(tikz_end, integer(0))) {
         print("Not a tikz file")
         return()
     }
     # Return Tikz data for a single tikz image
     # will not work on multiple tikz images
-    pre_tikz<- fig_data[seq_len(tikz_style-1)]
-    post_tikz<-fig_data[seq_len(tikz_end)]
-    tikz_data<-setdiff(post_tikz,pre_tikz)
-    print(paste("TKZ-S2 : Extracted tikz image data from  ",file_name))
+    pre_tikz <- fig_data[seq_len(tikz_style - 1)]
+    post_tikz <- fig_data[seq_len(tikz_end)]
+    tikz_data <- setdiff(post_tikz, pre_tikz)
+    print(paste("TKZ-S2 : Extracted tikz image data from  ", file_name))
     print(tikz_data)
     return(tikz_data)
 }
-inject_generated_image<-function(article_dir,file_name,image_path){
+inject_generated_image <- function(article_dir, file_name, image_path){
     tex_file <- readLines(file.path(article_dir, file_name))
     tikz_start <- which(grepl("^\\s*\\\\begin\\{tikzpicture", tex_file))
-    pre_tikz<- tex_file[seq_len(tikz_start-1)]
-    post_tikz<-setdiff(tex_file,pre_tikz)
-    include_graphics<-paste("\\includegraphics[scale=1]{",image_path,"}",sep='')
+    pre_tikz <- tex_file[seq_len(tikz_start - 1)]
+    post_tikz <- setdiff(tex_file, pre_tikz)
+    include_graphics <- paste("\\includegraphics[scale=1]{",
+                             image_path, "}" ,sep = "")
     # write to original wrapper file
-    write_file<-file(file_name,'w')
-    writeLines(c(pre_tikz,include_graphics,"",post_tikz), write_file)
+    write_file <- file(file_name, "w")
+    writeLines(c(pre_tikz,include_graphics, "", post_tikz), write_file)
     close(write_file)
 }
-pre_process_tikz<-function(article_dir){
-    wrapper_types=c('wrapper.tex','RJwrap.tex','RJwrapper.tex')
-    wrapper_file=""
-    for(w_type in wrapper_types){
-        if(file.exists(file.path(article_dir, w_type))) {
-            print(paste("Tikz-tools Stage 1 : Found ",w_type))
-            wrapper_file=w_type
-        }
-    }
-    file_name<-find_src_file(article_dir,wrapper_file)
-    if(!grepl(".tex$",file_name)){
-        file_name<-paste0(file_name,".tex")
-    }
-    abs_file_path<-tools::file_path_as_absolute(article_dir)
-    input_file<-file_name
-    latex_template<-system.file("extdata/latex.template", package = "texor")
-    tikz_filter<-system.file("extdata/extract_tikz_filter.lua", package = "texor")
-    pandoc_opt<-c(
-                  "--resource-path",abs_file_path,
-                  "--lua-filter",tikz_filter,
-                  "--template",latex_template)
-    input_format<-"latex+raw_tex"
-    output_format<-"latex"
-    out_file<-"outfile.tex" # temporary solution will change it later
-    rmarkdown::pandoc_convert(input_file,from = input_format,to= output_format,options=pandoc_opt,output = out_file,verbose = TRUE)
+pre_process_tikz <- function(article_dir){
+    input_file <- get_texfile_name(article_dir)
+    abs_file_path <- tools::file_path_as_absolute(article_dir)
+    latex_template <- system.file(
+                        "extdata/latex.template",
+                        package = "texor")
+    tikz_filter <- system.file(
+                        "extdata/extract_tikz_filter.lua",
+                        package = "texor")
+    pandoc_opt <- c(
+                  "--resource-path", abs_file_path,
+                  "--lua-filter", tikz_filter,
+                  "--template", latex_template)
+    input_format <- "latex+raw_tex"
+    output_format <- "latex"
+    out_file <- "outfile.tex" # temporary solution will change it later
+    rmarkdown::pandoc_convert(input_file,
+                              from = input_format,
+                              to = output_format,
+                              options = pandoc_opt,
+                              output = out_file,
+                              verbose = TRUE)
 }
-read_tikz_metadata<-function(article_dir){
-    wrapper_types=c('wrapper.tex','RJwrap.tex','RJwrapper.tex')
-    wrapper_file=""
-    for(w_type in wrapper_types){
-        if(file.exists(file.path(article_dir, w_type))) {
-            print(paste("Tikz-tools Stage 1 : Found ",w_type))
-            wrapper_file=w_type
-        }
-    }
-    file_name<-find_src_file(article_dir,wrapper_file)
-    if(!grepl(".tex$",file_name)){
-        file_name<-paste0(file_name,".tex")
-    }
-    src_file_data<- readLines(file.path(article_dir,file_name))
-    fig_start<- which(grepl("^\\s*\\\\begin\\{figure", src_file_data))
-    fig_end<- which(grepl("^\\s*\\\\end\\{figure", src_file_data))
-    pre_fig<- src_file_data[seq_len(fig_start)-1]
-    post_fig<-src_file_data[seq_len(fig_end)]
-    fig_data<-setdiff(post_fig,pre_fig)
-    caption_line<- which(grepl("^\\s*\\\\caption\\{", fig_data))
-    caption_data<- fig_data[caption_line]
-    caption_text<- gsub("\\\\caption\\{|","",caption_data)
-    caption_raw_text <- str_match(caption_text, "\\{\\s*(.*?)\\s*\\}")[,2]
-    label_line<- which(grepl("^\\s*\\\\label\\{", fig_data))
-    label_data<- fig_data[label_line]
-    label_raw_text<- gsub("fig:","",gsub("[[:space:]]", "",gsub("\\\\label\\{|\\}","",label_data)))
+
+read_tikz_metadata <- function(article_dir) {
+    file_name <- get_texfile_name(article_dir)
+    src_file_data <- readLines(file.path(article_dir, file_name))
+    fig_start <- which(grepl(
+            "^\\s*\\\\begin\\{figure", src_file_data))
+    fig_end <- which(grepl(
+            "^\\s*\\\\end\\{figure", src_file_data))
+    pre_fig <- src_file_data[seq_len(fig_start) - 1]
+    post_fig <- src_file_data[seq_len(fig_end)]
+    fig_data <- setdiff(post_fig,pre_fig)
+    caption_line <- which(grepl("^\\s*\\\\caption\\{", fig_data))
+    caption_data <- fig_data[caption_line]
+    caption_text <- gsub("\\\\caption\\{|", "", caption_data)
+    caption_raw_text <- str_match(caption_text, "\\{\\s*(.*?)\\s*\\}")[, 2]
+    label_line <- which(grepl("^\\s*\\\\label\\{", fig_data))
+    label_data <- fig_data[label_line]
+    label_raw_text <- gsub("fig:", "",
+                      gsub("[[:space:]]", "",
+                      gsub("\\\\label\\{|\\}", "", label_data)))
     # write caption and label into a temp_metadata file
-    fileConn<-file("tikz_label_meta.txt")
-    writeLines(paste(label_raw_text), fileConn)
-    close(fileConn)
-    fileConn<-file("tikz_caption_meta.txt")
-    writeLines(paste(caption_raw_text), fileConn)
-    close(fileConn)
+    tikz_label_file <- file("tikz_label_meta.txt")
+    writeLines(label_raw_text, tikz_label_file)
+    close(tikz_label_file)
+    tikz_caption_file <- file("tikz_caption_meta.txt")
+    writeLines(caption_raw_text, tikz_caption_file)
+    close(tikz_caption_file)
 }
