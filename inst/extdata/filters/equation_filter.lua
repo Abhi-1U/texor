cd58ac450ede88d5d3a644ce0952e981646b0d1b
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-08-24 15:20:35 +0530

    Equation references now supported

--- inst/extdata/filters/equation_filter.lua
+++ inst/extdata/filters/equation_filter.lua
@@ -8,6 +8,8 @@ Copyright: © 2022 Abhishek Ulayil
 License:   MIT – see LICENSE file for details
 --]]
 
+equation_labels = {}
+
 --[[
 Applies the filter to Math elements
 --]]
@@ -17,20 +19,43 @@ function Math(el)
         local text = pandoc.utils.stringify(el.text)
         --label = string.gsub(text, "\\label{^\\{(.-)\\}$}", "%1")
         s, e, l =string.find(text,"\\label{(.-)}")
+        table.insert(equation_labels,l)
+        -- Bookdown does not support period in equations hence substituting them as hyphen
+        l = string.gsub(l, "%.", "-")
         --label = string.gsub(label, "",)
-        print(l)
-        el.text = text .. [[(\#]] .. l .. [[)]]
+        --print(l)
+        el.text = text .. [[ (\#]] .. l .. [[)]]
         --print(text)
     else
         --pass
     end
-    local left = el.mathtype == 'InlineMath' and '\\(' or '$$'
+    local left = el.mathtype == 'InlineMath' and '\\(' or '\n$$'
     local right = el.mathtype == 'InlineMath' and '\\)' or '$$'
     return pandoc.RawInline('markdown', left .. el.text .. right)
     --print(el.text)
     --return(el)
 end
 
-
+function Link(el)
+    is_bkdwn = false
+    for _,label in pairs(equation_labels) do
+        if ("#"..label) == el.target then
+            print(label)
+            print(el.target)
+            local link_text = el.target
+            link_text = string.gsub(link_text, "%.", "-")
+            label = string.gsub(label, "%.", "-")
+            el.target = link_text
+            bkdwn = [[\@ref(]] .. label .. [[)]]
+            is_bkdwn = true
+            break
+        end
+    end
+    if is_bkdwn then
+        return pandoc.RawInline('markdown', bkdwn)
+    else
+        return(el)
+    end
+end
 
 --gsub("& ?= ?&?", "& =", x)
