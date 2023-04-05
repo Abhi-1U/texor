4a16b78df59eb8c8f77d9fa05ef9aac8ae248e97
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2023-04-05 23:12:49 +0530

    fixed spelling error

--- R/article-tools.R
+++ R/article-tools.R
@@ -153,7 +153,7 @@ generate_rmd <- function(article_dir) {
     }
 
     markdown_file <- paste(article_dir,xfun::with_ext(get_wrapper_type(article_dir),"md"),sep="/")
-    metadata <- rmarkdown::yaml_front_matter(markdown_filedown_file)
+    metadata <- rmarkdown::yaml_front_matter(markdown_file)
     # reads the abstract from the second author field
     # reason : abstract is patched as author in metafix.sty
     abstract <- metadata$author[2]
