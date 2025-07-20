0e5734e44373e4c55a8f1aa2b7eb440d528a513d
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2025-07-20 14:19:31 +0530

    Implementation improvements

--- R/stream-editor.R
+++ R/stream-editor.R
@@ -16,11 +16,7 @@ stream_editor <- function(raw_lines, pattern, target, replacement) {
     break_points <- which(grepl(pattern, raw_lines))
     #check for length of break_points to avoid no matches
     if (!identical(break_points, integer(0))) {
-        for (iterator in seq_along(break_points)) {
-            modification_line <- raw_lines[break_points[iterator]]
-            modified_line <- gsub(target, replacement, modification_line)
-            raw_lines[break_points[iterator]] <- modified_line
-        }
+        raw_lines[break_points] <-  gsub(target, replacement, raw_lines[break_points])
         message(paste(
             "Found ", length(break_points), "Matches for target :", target))
     } else {
--- R/string-tools.R
+++ R/string-tools.R
@@ -1,31 +1,19 @@
 #Wrapper functions for stringr package methods
 
 # -- stringr wrapper ----------------------------------------------------------
+## usethis namespace: start
+#' @importFrom stringr str_trim
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_match
+#' @importFrom stringr str_split
+#' @importFrom stringr str_length
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr fixed
+#' @importFrom stringr str_c
+## usethis namespace: end
+NULL
 
-str_trim <- function(x) {
-    return(stringr::str_trim(x))
-}
-str_detect <- function(x, re) {
-    return(stringr::str_detect(x, re))
-}
-str_match <- function(x, re) {
-    return(stringr::str_match(x,re))
-}
-str_split <- function(x,patt) {
-    return(stringr::str_split(x,patt))
-}
-str_length <- function(x) {
-    return(stringr::str_length(x))
-}
-str_replace_all <- function(x, patt, rep) {
-    return(stringr::str_replace_all(x, patt, rep))
-}
-fixed <- function(x) {
-    return(stringr::fixed(x))
-}
-str_c <- function(x, sep) {
-    return(stringr::str_c(x, sep))
-}
+
 empty <- function(x) UseMethod("empty")
 
 empty.character <- function(x) str_length(x) == 0
